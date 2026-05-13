4fbca30edb3a48bbbd0186a9ed9a43ad6db97237
Nagmacoder <84793179+Nagmacoder@users.noreply.github.com>
2026-05-13 14:20:23 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,15 @@
 import { Outlet } from "react-router-dom"
 import Header from './components/Header'
 import Footer from './components/Footer'
+import About from './components/About'
+
 
 const App = () => {
   return (
     <>
       <Header />
       <Outlet />
+      <About/>
       <Footer />
     </>
   )
